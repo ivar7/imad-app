55552edda0532c64726efae69477cf7474413a0e
ivar7 <gowdaravi98@gmail.com>
2017-08-23 00:29:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ function createTemplate(data){
     </html>`;
     return htmlTemplate;
 }
- var counter = 0;
+ 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -100,6 +100,8 @@ app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
+
+var counter = 0;
 
 app.get('/counter', function (req, res) {
     counter = counter + 1;
